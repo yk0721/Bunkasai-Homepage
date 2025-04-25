98c81491ac49629d73c396c67592e66767686b09
Kuma Yamashita <dredgk0@gmail.com>
2025-04-25 22:50:29 +0900

    Djangoの静的ファイルを無事に読み込めるようになりました。
    編集をするための準備が整いました。

--- Bunkasai_System/urls.py
+++ Bunkasai_System/urls.py
@@ -24,4 +24,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
